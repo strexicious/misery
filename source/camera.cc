f46e265b4fd198d677c17e8a34d44bd70e87e633
strexicious <strexicious@gmail.com>
2020-02-18 20:15:32 +0100

    understanding ref. systs. description (matrices)

--- source/camera.cc
+++ source/camera.cc
@@ -22,7 +22,22 @@ void Camera::set_angles(float x, float y) {
 }
 
 glm::mat4 Camera::view() const {
-    return glm::lookAt(pos, pos + forward, glm::vec3{0.f, 1.f, 0.f});
+    // ugh, you can see it as a rotation and transformation if you wish, :stinky:
+    // uses property of orthogonality
+    
+    glm::vec3 u_prime{0.0, 1.0, 0.0};
+    glm::vec3 s = glm::cross(forward, u_prime);
+    glm::vec3 up = glm::cross(s, forward);
+    
+    glm::mat4 res{
+        glm::vec4{s.x, up.x, -forward.x, 0.0},
+        glm::vec4{s.y, up.y, -forward.y, 0.0},
+        glm::vec4{s.z, up.z, -forward.z, 0.0},
+        glm::vec4{-glm::dot(s, pos), -glm::dot(up, pos), glm::dot(forward, pos), 1.0},
+    };
+
+    return res;
+    // return glm::lookAt(pos, pos + forward, glm::vec3{0.f, 1.f, 0.f});
 }
 
 glm::vec3 Camera::to_world_space(glm::vec3 v) const {
@@ -35,5 +50,22 @@ float Frustum::get_orig_w(float pixel_depth) const {
 }
 
 glm::mat4 Frustum::projection() const {
-    return glm::perspective(glm::radians(fovy), aspect_ratio, znear, zfar);
+    // the following matrix constructed is equivalent to glm's perspectiveRH_NO
+    // for OpenGL NDC cuz it's in the name: *gl*m
+    // Documentation from glm:
+    /// Creates a matrix for a right handed, symetric perspective-view frustum.
+	/// The near and far clip planes correspond to z normalized device coordinates of -1 and +1 respectively. (OpenGL clip volume definition)
+    
+    GLfloat proj_plane_d = 1 / glm::tan(glm::radians(fovy / 2));
+    // custom perspective matrix to project into OpenGL's NDC from a
+    // RHS camera looking towards -z axis system
+    glm::mat4 res{
+        glm::vec4{1.0/(aspect_ratio*proj_plane_d), 0.0, 0.0, 0.0},
+        glm::vec4{0.0, 1.0/proj_plane_d, 0.0, 0.0},
+        glm::vec4{0.0, 0.0, -(zfar+znear)/(zfar-znear), -1.0},
+        glm::vec4{0.0, 0.0, -2*zfar*znear/(zfar-znear), 0.0},
+    };
+    
+    return res;
+    // return glm::perspective(glm::radians(fovy), aspect_ratio, znear, zfar);
 }
